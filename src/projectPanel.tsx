34df1249b8ece892d67a62861b16ba262f734215
DenisaCG <denisa.checiu@gmail.com>
2024-08-02 13:03:57 +0200

    change render function to standalone React components

--- src/projectPanel.tsx
+++ src/projectPanel.tsx
@@ -48,12 +48,7 @@ const onSettingsClick = () => {
   window.open(url, '_blank', 'noreferrer');
 };
 
-export function ProjectPanel({
-  name,
-  description,
-  details,
-  environment
-}: IProjectProps) {
+function renderToolbar({ name }: IProjectProps) {
   const onShareClick = React.useCallback(() => {
     // open the sharing project dialog
     return showDialog({
@@ -68,143 +63,139 @@ export function ProjectPanel({
     });
   }, [name]);
 
-  const renderToolbar = () => {
-    return (
-      <div className={projectNavClass}>
-        <span className={spacer} />
-        <button
-          className={projectNavButtonClass}
-          title={'User Profile'}
-          type="button"
-          onClick={onProfileClick}
-        >
-          <ProfileIcon.react tag="span" className={projectNavIconClass} />
-        </button>
-        <button
-          className={projectNavButtonClass}
-          title={'Share Project'}
-          type="button"
-          onClick={onShareClick}
-        >
-          <ShareIcon.react tag="span" className={projectNavIconClass} />
-        </button>
-        <button
-          className={projectNavButtonClass}
-          title={'Project Settings'}
-          type="button"
-          onClick={onSettingsClick}
-        >
-          <SettingsIcon.react tag="span" className={projectNavIconClass} />
-        </button>
-      </div>
-    );
-  };
+  return (
+    <div className={projectNavClass}>
+      <span className={spacer} />
+      <button
+        className={projectNavButtonClass}
+        title={'User Profile'}
+        type="button"
+        onClick={onProfileClick}
+      >
+        <ProfileIcon.react tag="span" className={projectNavIconClass} />
+      </button>
+      <button
+        className={projectNavButtonClass}
+        title={'Share Project'}
+        type="button"
+        onClick={onShareClick}
+      >
+        <ShareIcon.react tag="span" className={projectNavIconClass} />
+      </button>
+      <button
+        className={projectNavButtonClass}
+        title={'Project Settings'}
+        type="button"
+        onClick={onSettingsClick}
+      >
+        <SettingsIcon.react tag="span" className={projectNavIconClass} />
+      </button>
+    </div>
+  );
+}
 
-  const renderName = () => {
-    return (
-      <div>
-        <button
-          disabled
-          className={projectCardClass}
-          title={'Project: ' + name}
-        >
-          <ProjectIcon.react tag="span" className={projectCardIconClass} />
-          <div className={projectCardTitleWrapperClass}>
-            <p className={projectCardSubtitleClass}>{'Project'}</p>
-            <p>{name}</p>
-          </div>
-        </button>
-      </div>
-    );
-  };
+function renderName({ name }: IProjectProps) {
+  return (
+    <div>
+      <button disabled className={projectCardClass} title={'Project: ' + name}>
+        <ProjectIcon.react tag="span" className={projectCardIconClass} />
+        <div className={projectCardTitleWrapperClass}>
+          <p className={projectCardSubtitleClass}>{'Project'}</p>
+          <p>{name}</p>
+        </div>
+      </button>
+    </div>
+  );
+}
 
-  const renderEnvironment = () => {
-    const onBuildEnvironmentClick = React.useCallback(() => {
-      EnvironmentDialog({
-        projectName: name,
-        envCode: environment.buildEnv,
-        envType: 'build'
-      });
-    }, [name, environment.buildEnv]);
+function renderEnvironment({ name, environment }: IProjectProps) {
+  const onBuildEnvironmentClick = React.useCallback(() => {
+    EnvironmentDialog({
+      projectName: name,
+      envCode: environment.buildEnv,
+      envType: 'build'
+    });
+  }, [name, environment.buildEnv]);
 
-    const onKernelEnvironmentClick = React.useCallback(() => {
-      EnvironmentDialog({
-        projectName: name,
-        envCode: environment.kernelEnv,
-        envType: 'kernel'
-      });
-    }, [name, environment.kernelEnv]);
-
-    return (
-      <div>
-        <button className={projectCardClass} title={'Environment'}>
-          <EnvironmentIcon.react tag="span" className={projectCardIconClass} />
-          <div className={projectCardTitleWrapperClass}>
-            <p className={projectCardSubtitleClass}>{'Environment'}</p>
-            <button
-              title={'Build environment'}
-              className={environmentButtonClass}
-              onClick={onBuildEnvironmentClick}
-            >
-              {'Build environment'}
-            </button>
-            <button
-              title={'Kernel environment'}
-              className={environmentButtonClass}
-              onClick={onKernelEnvironmentClick}
-            >
-              {'Kernel environment'}
-            </button>
-          </div>
-        </button>
-      </div>
-    );
-  };
-
-  const renderDescription = () => {
-    return (
-      <div>
-        <button
-          disabled
-          className={projectCardClass}
-          title={'Description: ' + description}
-        >
-          <DescriptionIcon.react tag="span" className={projectCardIconClass} />
-          <div className={projectCardTitleWrapperClass}>
-            <p className={projectCardSubtitleClass}>{'Description'}</p>
-            <p>{description}</p>
-          </div>
-        </button>
-      </div>
-    );
-  };
-
-  const renderDetails = () => {
-    return (
-      <div>
-        <button
-          disabled
-          className={projectCardClass}
-          title={'Details: ' + details}
-        >
-          <DetailsIcon.react tag="span" className={projectCardIconClass} />
-          <div className={projectCardTitleWrapperClass}>
-            <p className={projectCardSubtitleClass}>{'Details'}</p>
-            <p>{details}</p>
-          </div>
-        </button>
-      </div>
-    );
-  };
+  const onKernelEnvironmentClick = React.useCallback(() => {
+    EnvironmentDialog({
+      projectName: name,
+      envCode: environment.kernelEnv,
+      envType: 'kernel'
+    });
+  }, [name, environment.kernelEnv]);
 
   return (
+    <div>
+      <button className={projectCardClass} title={'Environment'}>
+        <EnvironmentIcon.react tag="span" className={projectCardIconClass} />
+        <div className={projectCardTitleWrapperClass}>
+          <p className={projectCardSubtitleClass}>{'Environment'}</p>
+          <button
+            title={'Build environment'}
+            className={environmentButtonClass}
+            onClick={onBuildEnvironmentClick}
+          >
+            {'Build environment'}
+          </button>
+          <button
+            title={'Kernel environment'}
+            className={environmentButtonClass}
+            onClick={onKernelEnvironmentClick}
+          >
+            {'Kernel environment'}
+          </button>
+        </div>
+      </button>
+    </div>
+  );
+}
+
+function renderDescription({ description }: IProjectProps) {
+  return (
+    <div>
+      <button
+        disabled
+        className={projectCardClass}
+        title={'Description: ' + description}
+      >
+        <DescriptionIcon.react tag="span" className={projectCardIconClass} />
+        <div className={projectCardTitleWrapperClass}>
+          <p className={projectCardSubtitleClass}>{'Description'}</p>
+          <p>{description}</p>
+        </div>
+      </button>
+    </div>
+  );
+}
+
+function renderDetails({ details }: IProjectProps) {
+  return (
+    <div>
+      <button
+        disabled
+        className={projectCardClass}
+        title={'Details: ' + details}
+      >
+        <DetailsIcon.react tag="span" className={projectCardIconClass} />
+        <div className={projectCardTitleWrapperClass}>
+          <p className={projectCardSubtitleClass}>{'Details'}</p>
+          <p>{details}</p>
+        </div>
+      </button>
+    </div>
+  );
+}
+
+export function ProjectPanel(props: IProjectProps) {
+  return (
     <React.Fragment>
-      {renderToolbar()}
+      {renderToolbar(props)}
       <div className={panelWrapperClass}>
-        {renderName()}
-        {renderDescription()}
-        {renderDetails()}
-        {renderEnvironment()}
+        {renderName(props)}
+        {renderDescription(props)}
+        {renderDetails(props)}
+        {renderEnvironment(props)}
       </div>
     </React.Fragment>
   );
